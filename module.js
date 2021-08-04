9b74da6aae36b2f4611cf8cadfdbb7c402afaea9
Manoj M <manoj904378@gmail.com>
2021-08-04 16:11:04 +0530

    Update module.js

--- module.js
+++ module.js
@@ -1,17 +1,23 @@
-const slider = (blogdata) => {
-  var title = blogdata.title ? blogdata.title : "";
-  var description = blogdata.description ? blogdata.description : "";
-  var url = blogdata.url ? blogdata.url : "";
-  var blogurl = blogdata.blogurl ? blogdata.blogurl : "";
-  var author = blogdata.author ? blogdata.author : "";
-  var published = blogdata.date ? blogdata.date : "";
-  var slider = document.querySelector(".slider");
-  var slidertag = document.createElement("div");
-  slidertag.classList.add("slider-container");
-  slidertag.innerHTML = `<div class="slider-title">${title}</div> <a href="${blogurl}"><img src="${url}" alt="${location}"></a> <div class="slider-description">${description}</div> <div class="post-details"> <div>${author}</div><div class="post-date">${published}</div> </div> `;
-  slider.append(slidertag);
+import BlogData, { greeting as greet } from "./posts.js";
+import Pagination from "./pagination.js";
+var currentPage = window.location.search.substr(1);
+var page = currentPage.split("=");
+var PageNo = parseInt(page[1]);
+var PageNo = PageNo ? PageNo : 1;
+const viewBlog = (blogdatas) => {
+  blogdatas.forEach((blogdata) => {
+    BlogData(blogdata);
+  });
+  greet();
 };
-export const greeting = () => {
-  console.log("Hello y'll i have some Foods you wanna know about them!");
-};
-export default slider;
+axios.get("./blog.json")
+  .then((response) => {
+    var datas = response.data;
+    var blogdatas = datas["page_" + PageNo];
+    viewBlog(blogdatas);
+    var TotalPage = datas["total_pages"];
+    Pagination(TotalPage, PageNo);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
