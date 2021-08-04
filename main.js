4d499561879e34134d181a747f50ecb25d794aa8
Manoj M <manoj904378@gmail.com>
2021-08-04 15:38:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
 import slider, { greeting as greet } from "./module.js";
-import blog from "./blog.json" assert { type: "json" };
-var blogdatas = blog.data;
-blogdatas.forEach((blogdata) => {
+import Pagination from "./pagination.js";
+var currentPage = window.location.search.substr(1);
+var page = currentPage.split("=");
+var PageNo = parseInt(page[1]);
+var PageNo = PageNo ? PageNo : 1;
+const viewBlog = (blogdatas) => {
+  blogdatas.forEach((blogdata) => {
     slider(blogdata);
-});
-greet();
+  });
+  greet();
+};
+axios
+  .get("./blog.json")
+  .then((response) => {
+    var datas = response.data;
+    var blogdatas = datas["page_" + PageNo];
+    viewBlog(blogdatas);
+    var TotalPage = datas["total_pages"];
+    Pagination(TotalPage, PageNo);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
