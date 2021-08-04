5cd5a1d4e7dcd433293a51be71261e1e194234c2
Manoj M <manoj904378@gmail.com>
2021-08-04 15:38:01 +0530

    Update module.js

--- module.js
+++ module.js
@@ -1,23 +1,17 @@
 const slider = (blogdata) => {
-    var title = blogdata.title ? blogdata.title : '';
-    var description = blogdata.description ? blogdata.description : '';
-    var url = blogdata.url ? blogdata.url : '';
-    var blogurl = blogdata.blogurl ? blogdata.blogurl : '';
-    var author = blogdata.author ? blogdata.author : '';
-    var published = blogdata.date ? blogdata.date : '';
-
-    var slider = document.querySelector('.slider');
-    var slidertag = document.createElement('div');
-    slidertag.classList.add('slider-container');
-    slidertag.innerHTML = `<div class="slider-title">${title}</div>
-                            <a href="${blogurl}"><img src="${url}" alt="${location}"></a>
-                            <div class="slider-description">${description}</div>
-                            <div>
-                                <div>${author}</div><div>${published}</div>
-                            </div> `;
-    slider.append(slidertag);
-}
+  var title = blogdata.title ? blogdata.title : "";
+  var description = blogdata.description ? blogdata.description : "";
+  var url = blogdata.url ? blogdata.url : "";
+  var blogurl = blogdata.blogurl ? blogdata.blogurl : "";
+  var author = blogdata.author ? blogdata.author : "";
+  var published = blogdata.date ? blogdata.date : "";
+  var slider = document.querySelector(".slider");
+  var slidertag = document.createElement("div");
+  slidertag.classList.add("slider-container");
+  slidertag.innerHTML = `<div class="slider-title">${title}</div> <a href="${blogurl}"><img src="${url}" alt="${location}"></a> <div class="slider-description">${description}</div> <div class="post-details"> <div>${author}</div><div class="post-date">${published}</div> </div> `;
+  slider.append(slidertag);
+};
 export const greeting = () => {
-    console.log("Hello y'll i have some Foods you wanna know about them!");
-}
+  console.log("Hello y'll i have some Foods you wanna know about them!");
+};
 export default slider;
